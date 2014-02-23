b83d49ad511785210d35530f1df89356636fb87b
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-02-23 17:17:37 +0100

    Improved tests.

--- src/NMCSTest.cpp
+++ src/NMCSTest.cpp
@@ -7,12 +7,11 @@ using namespace func;
 
 int main() {
 	//NO srand on purpose
-	unsigned int buffer[100];
-
 	for (int i = 1; i < 50; ++i) {
 		NMCS gen(i);
 		auto p = gen((rand() % i) % 2 + 1);
-		assert(p.second == countSquares(p.first, buffer));
+		assert(p.second == deterministicCountSquares(p.first));
+		assert((int)p.first.length() == i);
 	}
 
 	puts("OK");
